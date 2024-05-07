c1df26668a5602c2f34c58072c3aa23e8a42f4fe
Sagar Tandan <076bct072@ioepc.edu.np>
2024-05-07 19:42:58 +0545

    updated

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import joblib
 import uvicorn
 import numpy as np
+from pydantic import BaseModel
+
+class TextData(BaseModel):
+    texts: list[str]
 
 app = FastAPI(title="Sentiment Analysis API")
 
@@ -28,38 +32,54 @@ def get_sentiment_label(class_index):
     return labels[class_index]
 
 
-@app.get("/sentiment-analysis/{text}")
-def sentiment_analysis(text: str):
-    # blob = TextBlob(text)
-    # polarity = blob.sentiment.polarity
-    # subjectivity = blob.sentiment.subjectivity
 
-    # if polarity > 0:
-    #     sentiment = "positive"
-    # elif polarity < 0:
-    #     sentiment = "negative"
-    # else:
-    #     sentiment = "neutral"
+@app.post("/sentiment-analysis/")
+async def sentiment_analysis(data: TextData):
+    results = []
+    for text in data.texts:
+        tokenized_text = tokenizer.texts_to_sequences([text])
+        max_sequence_length = 250
+        padded = pad_sequences(tokenized_text, maxlen=max_sequence_length)
+        pred = model.predict(padded)
+        prediction = pred.tolist()[0]
+        final_prediction = prediction.index(max(prediction))
+        sentiment_label = get_sentiment_label(final_prediction)
+        results.append({"text": text,"predictions":prediction, "sentiment": sentiment_label})
+    return results
+
+
+# @app.get("/sentiment-analysis/{text}")
+# def sentiment_analysis(text: str):
+#     # blob = TextBlob(text)
+#     # polarity = blob.sentiment.polarity
+#     # subjectivity = blob.sentiment.subjectivity
+
+#     # if polarity > 0:
+#     #     sentiment = "positive"
+#     # elif polarity < 0:
+#     #     sentiment = "negative"
+#     # else:
+#     #     sentiment = "neutral"
         
-    tokenized_text = tokenizer.texts_to_sequences([text])
-    max_sequence_length = 250
+#     tokenized_text = tokenizer.texts_to_sequences([text])
+#     max_sequence_length = 250
 
-    padded = pad_sequences(tokenized_text, maxlen=max_sequence_length)
-    pred = model.predict(padded)
-    prediction = pred.tolist()[0]
-    finalPrediction = prediction.index(max(prediction))
-    sentiment_label = get_sentiment_label(finalPrediction)
-
-
-    prediction = model.summary()
+#     padded = pad_sequences(tokenized_text, maxlen=max_sequence_length)
+#     pred = model.predict(padded)
+#     prediction = pred.tolist()[0]
+#     finalPrediction = prediction.index(max(prediction))
+#     sentiment_label = get_sentiment_label(finalPrediction)
 
 
+#     prediction = model.summary()
 
 
 
-    return {
-        "text": text,
-        "prediction" : pred.tolist()[0],
-        "index" : finalPrediction,
-        "Emotion" : sentiment_label,
-    }
+
+
+#     return {
+#         "text": text,
+#         "prediction" : pred.tolist()[0],
+#         "index" : finalPrediction,
+#         "Emotion" : sentiment_label,
+#     }
