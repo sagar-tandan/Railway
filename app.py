8be49b9ce63639af167b5ea6214b07d277fc4cb3
Sagar Tandan <076bct072@ioepc.edu.np>
2024-05-07 14:39:17 +0545

    update

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+import json
 from textblob import TextBlob
 import uvicorn
 
 app = FastAPI(title="Sentiment Analysis API")
+model = load_model("LSTM78percentFinal.h5")
+
+# Load your tokenizer
+with open("tokenizer.json", "r") as f:
+    tokenizer_data = json.load(f)
+    tokenizer = json.loads(tokenizer_data)
+
 
 @app.get("/", include_in_schema=False)
 def index():
@@ -21,10 +32,20 @@ def sentiment_analysis(text: str):
         sentiment = "negative"
     else:
         sentiment = "neutral"
+        
+    tokenized_text = tokenizer.texts_to_sequences([text])
+    max_sequence_length = 250
+    tokenized_text = pad_sequences(tokenized_text, maxlen=max_sequence_length, padding='post')
+    prediction = model.predict(tokenized_text)
+
+
+
+
 
     return {
         "text": text,
         "sentiment": sentiment,
         "polarity": polarity,
-        "subjectivity": subjectivity
+        "subjectivity": subjectivity,
+        "prediction" : prediction,
     }
