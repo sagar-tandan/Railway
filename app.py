c103688bc610e3efda9300986457b3ba2823ba8b
Sagar Tandan <076bct072@ioepc.edu.np>
2024-05-07 14:26:27 +0545

    updated app.py

--- app.py
+++ app.py
@@ -3,42 +3,28 @@ from fastapi.responses import RedirectResponse
 from textblob import TextBlob
 import uvicorn
 
-app =  FastAPI(title="Sentiment Analysis API")
+app = FastAPI(title="Sentiment Analysis API")
 
 @app.get("/", include_in_schema=False)
-
 def index():
-
-    return RedirectResponse("/docs", status_code-308)
+    return RedirectResponse("/docs", status_code=308)
 
 @app.get("/sentiment-analysis/{text}")
-
 def sentiment_analysis(text: str):
-
-    blob =TextBlob(text)
-
-    polarity= blob.sentiment.polarity
-
-    subjectivity= blob.sentiment.subjectivity
-
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+    subjectivity = blob.sentiment.subjectivity
 
     if polarity > 0:
-        sentiment= "positive"
-
-
+        sentiment = "positive"
     elif polarity < 0:
-        sentiment= "negative"
-
+        sentiment = "negative"
     else:
-        sentiment= "neutral"
+        sentiment = "neutral"
 
     return {
         "text": text,
-
         "sentiment": sentiment,
-
         "polarity": polarity,
-
         "subjectivity": subjectivity
     }
-
